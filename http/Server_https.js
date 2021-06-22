eac20aebeb462663e318acc7e18ea8639aca7178
JongBuem <JongBuem>
2021-06-22 17:46:26 +0900

    update

--- http/Server_https.js
+++ http/Server_https.js
@@ -1,13 +1,13 @@
-var express = require("express");
-var http = require("http");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+  http = require("http");
+app = express();
+bodyParser = require("body-parser");
 var server = http.createServer(app);
-server.listen(80, () => {
-  console.log("시작");
-});
+server.listen(80);
+
 app.use(express.static(__dirname + "/web"));
 app.use(bodyParser.json());
+
 app.get("/", function (req, res) {
   console.log("[Server] GET : /");
   res.send("Hi, Client, I am a server");
