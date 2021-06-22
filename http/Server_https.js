a44804df02d1f128869ae3cae1c8cd1b66e9a439
JongBuem <JongBuem>
2021-06-22 10:42:37 +0900

    upload

---
+++ http/Server_https.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var http = require("http");
+var app = express();
+var bodyParser = require("body-parser");
+var server = http.createServer(app);
+server.listen(80);
+app.use(express.static(__dirname + "/web"));
+app.use(bodyParser.json());
+app.get("/", function (req, res) {
+  console.log("[Server] GET : /");
+  res.send("Hi, Client, I am a server");
+});
+app.post("/", (req, res) => {
+  console.log("[Server] POST : " + JSON.stringify(req.body));
+  res.send(`post value is : ` + req.body.Client + ``);
+});
